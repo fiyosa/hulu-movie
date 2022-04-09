fccd9cd8ca469e828b0a94957ae17ff08ff436c5
fiyosa <fiyosa97@gmail.com>
2022-04-09 13:58:03 +0700

    edit: error request

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,10 +1,14 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
+import { forwardRef } from "react";
 
-const Thumbnail = ({ result }: any) => {
+const Thumbnail = forwardRef(({ result }: any, ref: any) => {
   const BASE_URL: string = `https://image.tmdb.org/t/p/original/`;
   return (
-    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -28,6 +32,6 @@ const Thumbnail = ({ result }: any) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
--- utils/request.tsx
+++ utils/request.tsx
@@ -1,6 +1,12 @@
 const API_KEY = process.env.API_KEY;
 
-const myAPI = {
+interface APIOptions {
+  [key: string]: {
+    title: string;
+    url: string;
+  };
+}
+const myAPI: APIOptions = {
   fetchTrending: {
     title: "Trending",
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
