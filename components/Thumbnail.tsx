d1f63258edea743c28d083e18cbd8625b554e4e2
fiyosa <fiyosa97@gmail.com>
2022-04-09 12:51:59 +0700

    edit: error ref

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,14 +1,10 @@
 import { ThumbUpIcon } from "@heroicons/react/outline";
 import Image from "next/image";
-import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref: any) => {
+const Thumbnail = ({ result }: any) => {
   const BASE_URL: string = `https://image.tmdb.org/t/p/original/`;
   return (
-    <div
-      ref={ref}
-      className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50"
-    >
+    <div className="p-2 group cursor-pointer transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
         src={
@@ -32,6 +28,6 @@ const Thumbnail = forwardRef(({ result }, ref: any) => {
       </div>
     </div>
   );
-});
+};
 
 export default Thumbnail;
